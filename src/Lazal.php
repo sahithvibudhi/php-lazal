0e46c76cc0c585204c9f3a3cf39dc36494c4bbfd
vsahith <sahit.v@darwinbox.io>
2018-07-17 12:04:10 +0530

    lazal connection close added

--- src/Lazal.php
+++ src/Lazal.php
@@ -1,5 +1,8 @@
-<?php namespace Lazal\Client;
+<?php 
 
+namespace Lazal;
+
+use Exception;
 /**
 *  A sample class
 *
@@ -8,7 +11,7 @@
 *
 *  @author yourname
 */
-class Lazal{
+class Client {
 
    /**  @var string $m_SampleProperty define here what this variable is for, do this for every instance variable */
    private $_sock = '';
@@ -23,47 +26,55 @@ class Lazal{
   */
    public function __construct($params){
 
-        $address = $params[$address];
-        $port = $params[$port];
+        $address = $params['host'];
+        $service_port = $params['port'];
         $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
         if ($socket === false) {
             throw new Exception("socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n");
         } 
-
-        $result = socket_connect($socket, $address, $service_port);
-        if ($result === false) {
-            throw new Exception("socket_connect() failed.\nReason: ($result) " . socket_strerror(socket_last_error($socket)) . "\n");
-        } 
+        try {
+          $result = socket_connect($socket, $address, $service_port);
+          if ($result === false) {
+              throw new Exception("socket_connect() failed.\nReason: ($result) " . socket_strerror(socket_last_error($socket)) . "\n");
+          } 
+        }catch(Exception $e) {
+          throw new Exception("Could not establish connection with Lazal");
+        }
         $this->_sock = $socket;
     
    }
 
    public function set($key, $val) {
-        $this->sockWrite("SET $key $val");
+        $this->sockWrite("SET $key $val \r");
         return (bool)$this->sockRead();
    }
 
    public function get($key) {
-        $this->sockWrite("GET $key");
+        $this->sockWrite("GET $key \r");
         return $this->sockRead();
    }
 
    public function delete($key) {
-        $this->sockWrite("DEL $key");
+        $this->sockWrite("DEL $key \r");
         return $this->sockRead();
    }
 
    private function sockWrite($command) {
-        socket_write($socket, $command, strlen($in));
+        socket_write($this->_sock, $command, strlen($command));
    }
 
    private function sockRead() {
         $r = '';
-        while ($out = socket_read($this->sock, 2048)) {
-            $r .= $output;
+        while ($out = socket_read($this->_sock, 2048)) {
+            $r .= $out;
+            if (strpos($out, "\n") !== false) break;
         }
         return $r;
+   }
+
+   public function close() {
+        socket_close($this->_sock);
    }
 
 }
